1bf51a89b18caafb19d1505ec443e3a4f478c7e9
Dhaval Khandhadia <40696547+dhavalkhandhadia@users.noreply.github.com>
2026-05-14 22:03:56 +0530

    Create form3_part2.cpp

---
+++ DP/Form3/form3_part2.cpp
@@ -0,0 +1,70 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int long long 
+
+#define endl '\n'
+
+int n;
+string s,t;
+
+int dp[1001][1001];
+
+
+int rec(int i, int j){
+    int ans = 0;
+    //pruning 
+
+    //base case
+    if(i==n || j==n){
+        return 0;
+    }
+    //cache
+    if(dp[i][j]!=-1)return dp[i][j];
+    //compute
+    if(s[i]==t[j]){
+        ans = rec(i+1,j+1)+1;
+
+    }else{
+        ans = max(rec(i+1,j),rec(i,j+1));
+    }
+
+    dp[i][j] = ans;
+    return ans;
+    //save and return
+
+}
+
+void solve(){
+    cin>>n;
+    cin>>s>>t;
+    memset(dp,-1,sizeof(dp));
+    int ans = 0;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            ans = max(ans, rec(i,j));
+        }
+    } 
+
+    // or 
+    // rec(0,0);
+
+    cout<<ans;
+
+}
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int t;
+    // cin>>t;
+
+    t = 1;
+    while(t--){
+        solve();
+    }
+
+
+    return 0;
+}
